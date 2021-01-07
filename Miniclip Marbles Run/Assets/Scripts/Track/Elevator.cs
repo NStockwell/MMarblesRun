fd253b2aae0e2f0b40df0007147279a66b1f5c7e
Nuno Moreira <nuno.moreira@miniclip.com>
2021-01-07 14:57:52 +0000

    Elevator

---
+++ Miniclip Marbles Run/Assets/Scripts/Track/Elevator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public float TimeToClimb;
+
+    public int NumPaddles;
+
+    public GameObject originalPaddle;
+    
+    public Transform start;
+    public Transform end;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < NumPaddles; i++)
+        {
+            Paddle paddle = Instantiate(originalPaddle).GetComponent<Paddle>();
+            paddle.delay = i * TimeToClimb / NumPaddles;
+            paddle.start = start;
+            paddle.end = end;
+        }
+    }
+}
---
+++ Miniclip Marbles Run/Assets/Scripts/Track/Paddle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float interval;
+    public float delay;
+    
+    private float elapsedTime;
+    
+    public Transform start;
+    public Transform end;
+
+    private MeshRenderer meshRenderer;
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (delay > 0)
+        {
+            delay -= Time.fixedDeltaTime;
+            if(delay > 0)
+                return;
+        }
+
+        if (delay < 0)
+        {
+            elapsedTime = -delay;
+            delay = 0;
+            meshRenderer.enabled = true;
+        }
+        else
+        {
+            meshRenderer.enabled = true;
+            elapsedTime += Time.fixedDeltaTime;    
+        }
+        
+        if (elapsedTime >= interval)
+        {
+            elapsedTime -= interval;
+        }
+        
+        transform.position = Vector3.Lerp(start.position, end.position, elapsedTime / interval);
+    }
+}
