81dfc5e47d03ad6e5dcdfc1e47ad0f0a20616eb8
Nuno Moreira <nuno.moreira@miniclip.com>
2021-01-07 11:40:46 +0000

    Marble

---
+++ Miniclip Marbles Run/Assets/Scripts/Marble/Marble.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Marble : MonoBehaviour
+{
+   public int ID;
+   private Rigidbody rigidBody;
+
+   public Rigidbody Rigidbody => rigidBody;
+
+   private void Awake()
+   {
+      rigidBody = GetComponent<Rigidbody>();
+   }
+}
