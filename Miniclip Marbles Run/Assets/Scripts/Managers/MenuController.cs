1e2d3e34483d63916a8e4a4a7d92cf128b2c5d1e
João Seixas <joao.seixas@miniclip.com>
2021-01-07 18:03:01 +0000

    Add Start Scene

---
+++ Miniclip Marbles Run/Assets/Scripts/Managers/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    private static MenuController _instance;
+
+    public static MenuController Instance
+    {
+        get => _instance;
+    }
+
+    public Button PlayButton;
+    
+    private void Awake() 
+    { 
+        if (_instance != null && _instance != this) 
+        { 
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    } 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayButton.onClick.AddListener(() => SceneManager.LoadScene("SampleSceneSeixas"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
