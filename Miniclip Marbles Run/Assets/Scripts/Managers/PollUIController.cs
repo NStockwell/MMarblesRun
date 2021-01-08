4c0e98cc806b8ebac0d38258d26cc5191cb0e369
João Seixas <joao.seixas@miniclip.com>
2021-01-08 11:58:20 +0000

    Add PollUI to HUD
    Currently it's hard to test since actions are being done right after first input

---
+++ Miniclip Marbles Run/Assets/Scripts/Managers/PollUIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PollUIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Marble myMarble;
+    public Slider Speed; // C
+    public Slider Left; // A
+    public Slider Right; // B
+    public Slider Break; // D
+    
+    void Start()
+    {
+        myMarble = RaceManager.Instance.marble1.GetComponent<Marble>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Poll poll = PollManager.Instance.polls[myMarble.ID];
+        Speed.value = poll.CVotesPercentage;
+        Left.value = poll.AVotesPercentage;
+        Right.value = poll.BVotesPercentage;
+        Break.value = poll.DVotesPercentage;
+    }
+}
