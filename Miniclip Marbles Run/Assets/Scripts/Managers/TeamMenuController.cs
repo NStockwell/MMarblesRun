6dced35d207caa600b297d609d9ec9ba02a7ec25
João Seixas <joao.seixas@miniclip.com>
2021-01-08 11:00:27 +0000

    Team Selection yei!

--- Miniclip Marbles Run/Assets/Scripts/Managers/TeamMenuController.cs
+++ Miniclip Marbles Run/Assets/Scripts/Managers/TeamMenuController.cs
@@ -5,24 +5,43 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
 
 public class TeamMenuController : MonoBehaviour
 {
     public Button LeftScroller;
     public Button RightScroller;
     public Button PlayButton;
-    public ScrollRect ScrollView;
 
     private int _currentNavIndex = 0;
-    
+    private readonly Color k_orange = new Color(1, 0.64F, 0, 1);
+
+    private GameObject[] _teams = new GameObject[8];
+
     // Start is called before the first frame update
     void Start()
     {
         LeftScroller.gameObject.SetActive(false);
         LeftScroller.onClick.AddListener(OnClickLeft);
+        LeftScroller.gameObject.SetActive(false);
         RightScroller.onClick.AddListener(OnClickRight);
         PlayButton.onClick.AddListener(OnClickPlayButton);
-        ScrollView.onValueChanged.AddListener(OnValueChangedScrollView);
+
+        _teams[0] = GameObject.Find("Team1");
+        _teams[1] = GameObject.Find("Team2");
+        _teams[2] = GameObject.Find("Team3");
+        _teams[3] = GameObject.Find("Team4");
+        _teams[4] = GameObject.Find("Team5");
+        _teams[4].SetActive(false);
+        _teams[5] = GameObject.Find("Team6");
+        _teams[5].SetActive(false);
+        _teams[6] = GameObject.Find("Team7");
+        _teams[6].SetActive(false);
+        _teams[7] = GameObject.Find("Team8");
+        _teams[7].SetActive(false);
+
+        Image img = _teams[0].GetComponent<Image>();
+        img.color = k_orange;
     }
 
     // Update is called once per frame
@@ -31,23 +50,154 @@ public class TeamMenuController : MonoBehaviour
         
     }
 
+    private void RotateLeft()
+    {
+        if (_currentNavIndex <= 3)
+        {
+            return;
+        }
+
+        switch (_currentNavIndex)
+        {
+            case 4:
+                _teams[4].transform.position = _teams[3].transform.position;
+                _teams[4].SetActive(true);
+                _teams[3].transform.position = _teams[2].transform.position;
+                _teams[2].transform.position = _teams[1].transform.position;
+                _teams[1].transform.position = _teams[0].transform.position;
+                _teams[0].SetActive(false);
+                break;
+            case 5:
+                _teams[5].transform.position = _teams[4].transform.position;
+                _teams[5].SetActive(true);
+                _teams[4].transform.position = _teams[3].transform.position;
+                _teams[3].transform.position = _teams[2].transform.position;
+                _teams[2].transform.position = _teams[1].transform.position;
+                _teams[1].SetActive(false);
+                break;
+            case 6:
+                _teams[6].transform.position = _teams[5].transform.position;
+                _teams[6].SetActive(true);
+                _teams[5].transform.position = _teams[4].transform.position;
+                _teams[4].transform.position = _teams[3].transform.position;
+                _teams[3].transform.position = _teams[2].transform.position;
+                _teams[2].SetActive(false);
+                break;
+            case 7:
+                _teams[7].transform.position = _teams[6].transform.position;
+                _teams[7].SetActive(true);
+                _teams[6].transform.position = _teams[5].transform.position;
+                _teams[5].transform.position = _teams[4].transform.position;
+                _teams[4].transform.position = _teams[3].transform.position;
+                _teams[3].SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    private void RotateRight()
+    {
+        if (_currentNavIndex < 3)
+        {
+            return;
+        }
+
+        switch (_currentNavIndex)
+        {
+            case 3:
+                _teams[0].transform.position = _teams[1].transform.position;
+                _teams[0].SetActive(true);
+                _teams[1].transform.position = _teams[2].transform.position;
+                _teams[2].transform.position = _teams[3].transform.position;
+                _teams[3].transform.position = _teams[4].transform.position;
+                _teams[4].SetActive(false);
+                break;
+            case 4:
+                _teams[1].transform.position = _teams[2].transform.position;
+                _teams[1].SetActive(true);
+                _teams[2].transform.position = _teams[3].transform.position;
+                _teams[3].transform.position = _teams[4].transform.position;
+                _teams[4].transform.position = _teams[5].transform.position;
+                _teams[5].SetActive(false);
+                break;
+            case 5:
+                _teams[2].transform.position = _teams[3].transform.position;
+                _teams[2].SetActive(true);
+                _teams[3].transform.position = _teams[4].transform.position;
+                _teams[4].transform.position = _teams[5].transform.position;
+                _teams[5].transform.position = _teams[6].transform.position;
+                _teams[6].SetActive(false);
+                break;
+            case 6:
+                _teams[3].transform.position = _teams[4].transform.position;
+                _teams[3].SetActive(true);
+                _teams[4].transform.position = _teams[5].transform.position;
+                _teams[5].transform.position = _teams[6].transform.position;
+                _teams[6].transform.position = _teams[7].transform.position;
+                _teams[7].SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+
     private void OnClickLeft()
     {
-        
+        ReduceIndex();
+        RotateRight();
     }
 
     private void OnClickRight()
     {
-        
+        AdvanceIndex();
+        RotateLeft();
     }
     
     private void OnClickPlayButton()
     {
-        PlayButton.onClick.AddListener(() => SceneManager.LoadScene("SampleSceneSeixas"));
+        SceneManager.LoadScene("SampleSceneSeixas");
     }
 
-    public void OnValueChangedScrollView(Vector2 value)
+    private void AdvanceIndex()
     {
-        Debug.Log("ListenerMethod: " + value);
+        if (_currentNavIndex >= 0 && !LeftScroller.gameObject.activeSelf)
+        {
+            LeftScroller.gameObject.SetActive(true);
+        }
+
+        if (_currentNavIndex == 6)
+        {
+            RightScroller.gameObject.SetActive(false);
+        }
+
+        Image currentImage = _teams[_currentNavIndex].GetComponent<Image>();
+        currentImage.color = Color.white;
+
+        _currentNavIndex++;
+
+        Image nextImage = _teams[_currentNavIndex].GetComponent<Image>();
+        nextImage.color = k_orange;
+    }
+    
+    private void ReduceIndex()
+    {
+        if (_currentNavIndex <= 7 && !RightScroller.gameObject.activeSelf)
+        {
+            RightScroller.gameObject.SetActive(true);
+        }
+
+        if (_currentNavIndex == 1)
+        {
+            LeftScroller.gameObject.SetActive(false);
+        }
+        
+        Image currentImage = _teams[_currentNavIndex].GetComponent<Image>();
+        currentImage.color = Color.white;
+
+        _currentNavIndex--;
+
+        Image nextImage = _teams[_currentNavIndex].GetComponent<Image>();
+        nextImage.color = k_orange;
     }
 }
